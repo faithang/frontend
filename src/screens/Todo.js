3609a7440aa8154bd2723c845a349776ea8f753f
russell_chua <russell_chua@tech.gov.sg>
2023-06-10 13:06:55 +0800

    Add checkpoint 0: todo app interface

--- src/screens/Todo.js
+++ src/screens/Todo.js
@@ -1,144 +1,12 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
 import { Container, Button, Form, FormCheck } from "@govtechsg/sgds-react";
-import CONFIG from "../config";
 import Table from "../components/Table";
-import crossIcon from "../icons/cross.svg";
-
-// TodoItem element that manages updates and deletion of todo items
-// Props will hold todoItems passed from Todo Element
-function TodoItem(props) {
-  const [done, setDone] = useState(props.done);
-
-  // PUT request to update todo when marked completed
-  const updateTodoItem = (done) => {
-    setDone(done)
-    axios.put(`${CONFIG.API_ENDPOINT}/${props.id}`, {
-      id: props.id,
-      description: props.description,
-      done: done,
-    });
-  }
-
-  // DELETE request to remove entry
-  const deleteTodoItem = () => {
-    axios.delete(`${CONFIG.API_ENDPOINT}/${props.id}`)
-      .then(() => {
-        // Calls for re-render of component after deletion
-        props.refreshToDos();
-      })
-  }
-
-  return (
-    <>
-      <tr>
-        <td>
-          <FormCheck
-            onChange={(event) => updateTodoItem(event.currentTarget.checked)}
-            checked={done}
-          />
-        </td>
-        <td width={"100%"}>{props.description}</td>
-        <td>
-          <img
-            alt="delete-icon"
-            src={crossIcon}
-            onClick={deleteTodoItem}
-            className="delete-icon"
-          />
-        </td>
-      </tr>
-    </>
-  );
-}
-
-// TodoItem element that manages updates and deletion of todo items
-// Props will hold todoItems passed from Todo Element
-function TodoItem(props) {
-  const [done, setDone] = useState(props.done);
-
-  // PUT request to update todo when marked completed
-  const updateTodoItem = (done) => {
-    setDone(done)
-    axios.put(`${CONFIG.API_ENDPOINT}/${props.id}`, {
-      id: props.id,
-      description: props.description,
-      done: done,
-    });
-  }
-
-  // DELETE request to remove entry
-  const deleteTodoItem = () => {
-    axios.delete(`${CONFIG.API_ENDPOINT}/${props.id}`)
-      .then(() => {
-        // Calls for re-render of component after deletion
-        props.refreshToDos();
-      })
-  }
-
-  return (
-    <>
-      <tr>
-        <td>
-          <FormCheck
-            onChange={(event) => updateTodoItem(event.currentTarget.checked)}
-            checked={done}
-          />
-        </td>
-        <td width={"100%"}>{props.description}</td>
-        <td>
-          <img
-            alt="delete-icon"
-            src={crossIcon}
-            onClick={deleteTodoItem}
-            className="delete-icon"
-          />
-        </td>
-      </tr>
-    </>
-  );
-}
 
 function Todo() {
-  const [todoItems, setTodoItems] = useState({});
-  const [newTodoDescription, setNewTodoDescription] = useState("");
-
   const today = new Date();
   const dateOptions = {
     day: "numeric",
     month: "long",
     year: "numeric",
-  }
-
-  // Manages side effects like API calls
-  useEffect(() => {
-    // Fetches data on initial render
-    populateTodos();
-  }, [todoItems]); // Add variables to dependency array to allow re-render of information on state changes
-
-  // GET request to fetch ToDo information 
-  const populateTodos = () => {
-    axios.get(`${CONFIG.API_ENDPOINT}`)
-      .then((result) => {
-        setTodoItems(result.data);
-      })
-  }
-
-  // POST request to submit new ToDo entry
-  const submitNewTodo = () => {
-    // Validation to ensure entry is not empty
-    if (newTodoDescription.trim() !== "") {
-      const newTodo = {
-        description: newTodoDescription,
-      };
-      axios.post(`${CONFIG.API_ENDPOINT}`, newTodo).then(() => {
-        // Does below action after request has been made 
-        populateTodos();
-        setNewTodoDescription("");
-      })
-    } else {
-      alert("Invalid Todo input!");
-    }
   }
 
   return (
@@ -156,17 +24,6 @@ function Todo() {
             </tr>
           </thead>
           <tbody>
-            {/* Iterate todoItems list to create new rows */}
-            {Object.keys(todoItems).map((item) => (
-              // Forwards items to TodoItem element as props
-              <TodoItem
-                key={todoItems[item].id}
-                id={todoItems[item].id}
-                done={todoItems[item].done}
-                description={todoItems[item].description}
-                refreshToDos={populateTodos}
-              />
-            ))}
             <tr>
               <td>
                 <FormCheck disabled />
@@ -177,17 +34,13 @@ function Todo() {
                   placeholder="Enter new to-do here"
                   id="newTodoDescription"
                   type="text"
-                  value={newTodoDescription}
-                  onChange={(event) => {
-                    setNewTodoDescription(event.currentTarget.value);
-                  }}
                 ></input>
               </td>
             </tr>
           </tbody>
         </Table>
       </Form>
-      <Button size="sm" variant="primary" onClick={submitNewTodo}>
+      <Button size="sm" variant="primary">
         Add
       </Button>
     </Container>
