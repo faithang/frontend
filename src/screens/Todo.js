61d79b2f08cc8f9eacbb085fa95f82a20a01889c
russell_chua <russell_chua@tech.gov.sg>
2023-06-10 12:40:51 +0800

    Add checkpoint 0: todo app interface

--- src/screens/Todo.js
+++ src/screens/Todo.js
@@ -1,50 +1,12 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
 import { Container, Button, Form, FormCheck } from "@govtechsg/sgds-react";
-import CONFIG from "../config";
 import Table from "../components/Table";
 
-// TodoItem element that manages updates and deletion of todo items
-// Props will hold todoItems passed from Todo Element
-function TodoItem(props) {
-  const [done, setDone] = useState(props.done);
-
-  return (
-    <>
-      <tr>
-        <td>
-          <FormCheck
-            checked={done}
-          />
-        </td>
-        <td width={"100%"}>{props.description}</td>
-      </tr>
-    </>
-  );
-}
-
 function Todo() {
-  const [todoItems, setTodoItems] = useState({});
-
   const today = new Date();
   const dateOptions = {
     day: "numeric",
     month: "long",
     year: "numeric",
-  }
-
-  // Manages side effects like API calls
-  useEffect(() => {
-    // Fetches data on initial render
-    populateTodos();
-  }, [todoItems]); // Add variables to dependency array to allow re-render of information on state changes
-
-  // GET request to fetch ToDo information 
-  const populateTodos = () => {
-    axios.get(`${CONFIG.API_ENDPOINT}`)
-      .then((result) => {
-        setTodoItems(result.data);
-      })
   }
 
   return (
@@ -62,17 +24,6 @@ function Todo() {
             </tr>
           </thead>
           <tbody>
-            {/* Iterate todoItems list to create new rows */}
-            {Object.keys(todoItems).map((item) => (
-              // Forwards items to TodoItem element as props
-              <TodoItem
-                key={todoItems[item].id}
-                id={todoItems[item].id}
-                done={todoItems[item].done}
-                description={todoItems[item].description}
-                refreshToDos={populateTodos}
-              />
-            ))}
             <tr>
               <td>
                 <FormCheck disabled />
